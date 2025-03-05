b692c2fcf3fc6dc3102e20a4367e377d42c6cebe
natkhatishigh <iamshivanshsood@gmail.com>
2025-03-05 21:34:29 +0530

    this is custom commit message

--- Arrays/hehe.java
+++ Arrays/hehe.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class hehe{
+    //testing custom commit message
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
