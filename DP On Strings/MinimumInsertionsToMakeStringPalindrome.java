3b895bbded3e3148a9bbae8ece682e079e9a4c36
natkhatishigh <iamshivanshsood@gmail.com>
2025-03-17 17:06:26 +0530

    Completed Minimum Insertion Steps To Make String Palindrome

---
+++ DP On Strings/MinimumInsertionsToMakeStringPalindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestPalindromicSub(String s){
+        String rev = new StringBuilder(s).reverse().toString();
+        char[] str1 = s.toCharArray();
+        char[] str2 = rev.toCharArray();
+
+        int n = str1.length;
+        int m = str2.length;
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int j = 0; j < m; j++) dp[0][j] = 0;
+        for(int i = 0; i < n; i++) dp[i][0] = 0;
+
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = Math.max(ans, dp[i][j]);
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return ans;
+    }
+    public int minInsertions(String s) {
+        return s.length() - longestPalindromicSub(s);
+    }
+}
