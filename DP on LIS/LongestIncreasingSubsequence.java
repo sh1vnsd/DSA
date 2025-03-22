7fe50476ef060cd14b5f2769b4a2769a81650248
natkhatishigh <iamshivanshsood@gmail.com>
2025-03-22 18:31:55 +0530

    Completed Longest Increasing Subsequence

--- DP on LIS/LongestIncreasingSubsequence.java
+++ DP on LIS/LongestIncreasingSubsequence.java
@@ -62,3 +62,24 @@ class Solution {
     }
 }
 
+
+
+//Another Tabulation
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int maxi = 1;
+        for(int ind = 0; ind < n; ind++){
+            for(int prev = 0; prev < ind; prev++){
+                if(nums[prev] < nums[ind]){
+                    dp[ind] = Math.max(1 + dp[prev], dp[ind]);
+                }
+            }
+            maxi = Math.max(maxi, dp[ind]);
+        }
+        return maxi;
+
+    }
+}
