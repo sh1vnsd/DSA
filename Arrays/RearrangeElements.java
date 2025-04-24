6133620580041ae1d9b6b11edf143c002f583397
natkhatishigh <iamshivanshsood@gmail.com>
2025-04-24 17:54:46 +0530

    Completed Rearrange Elements

---
+++ Arrays/RearrangeElements.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int pos = 0, neg = 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] < 0){
+                ans[neg] = nums[i];
+                neg = neg + 2;
+            }
+            else{
+                ans[pos] = nums[i];
+                pos = pos + 2;
+            }
+        }
+        return ans;
+    }
+}
