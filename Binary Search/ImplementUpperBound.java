46b6470452e5cc97ff74a17a9cb61e4e3cb38386
natkhatishigh <iamshivanshsood@gmail.com>
2025-05-01 17:16:15 +0530

    Completed Implement Upper Bound

---
+++ Binary Search/ImplementUpperBound.java
@@ -0,0 +1,21 @@
+class Solution {
+    int upperBound(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        int low = 0, high = n - 1, ans = n;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] > target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
