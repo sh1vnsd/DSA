68d289b2b8e247317b4ad0c91f95283041ca11cc
natkhatishigh <iamshivanshsood@gmail.com>
2025-07-03 14:10:37 +0530

    Completed Next Greater Element Two

---
+++ Stack/NextGreaterElementTwo.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+
+        int[] ans = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 2 * n - 1; i >= 0; i--){
+
+            while(!st.isEmpty() && st.peek() <= arr[i % n]){
+                st.pop();
+            }
+
+            if(i < n){
+                ans[i] = st.isEmpty() ? -1 : st.peek();
+            }
+
+            st.push(arr[i % n]);
+        }
+
+        return ans;
+    }
+}
