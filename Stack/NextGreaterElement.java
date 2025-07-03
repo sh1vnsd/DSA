0b7d942f844b6294b54747c0d223a9e0c5a98276
natkhatishigh <iamshivanshsood@gmail.com>
2025-07-03 13:48:08 +0530

    Completed Next Greater Element

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        
+        for(int i = n - 1; i >= 0; i--){
+
+            while(!st.isEmpty() && st.peek() < arr[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans.add(-1);
+            }
+            else{
+                ans.add(st.top());
+            }
+
+            st.push(arr[i]);
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
